9a7a2014c47e539f00d02e2971174bda878e0d5f
zyy <1076192792@QQ.com>
2025-11-19 18:41:41 +0800

    Revert "use these file as etc"
    This reverts commit 7f42039e09b05705cbda073c8661b72c7348c100.

--- conf/freebsd_conf/usr/bin/gdb-run-noninteractive.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# This is the same as gdb-run.sh but non-interative (so can be used when running tests/benchmarks, etc.)
-GDB="${GDB:-gdb} --quiet --batch --return-child-result" gdb-run.sh "$@"
